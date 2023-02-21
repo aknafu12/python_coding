0be2a9b088a9fd8909638f8349b0a75be6de829e
aknafu12 <kgebre2021@gmail.com>
2023-02-21 14:31:24 -0500

    added the actual solution

--- rectangle_area.py
+++ rectangle_area.py
@@ -1,6 +1,8 @@
-#coding challege from codingninjas.com site
-def  area_rectangle(length,breadth):  
-    
+#coding challege from codingninjas.com site:mycode
+#give test case five
+#test case1 : 4 5
+#test case2 : 55 10
+def  area_rectangle(length,breadth):      
     return length*breadth  
 
 test_case_value = [4,5,55,10,63,52,400,6431,43,65]
@@ -9,6 +11,28 @@ for x in range(5):
     print(area_rectangle(test_case_value[x+0],test_case_value[x+1]))
     #remove used arguments
     test_case_value = test_case_value[1:]
+#actual solution
+ 
+#Taking length and breadth as input
+l,b = map(float,input().split(' '))
+
+#Calculating area
+area = l * b
+
+#print the area
+print(int(area))
+
+
+
+
+
+
+
+
+
+
+ 
+
     #print(test_case_list)
 #nameerror a=b
 #valuError a =int('hello')
@@ -19,6 +43,7 @@ for x in range(5):
 #print(area_rectangle(55,10))
 #Your code goes here
 #Your code goes here
+
 
 name = 'John'
 age = 25
